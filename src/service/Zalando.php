85c99a541e850acb705ef2018b4060265c3e54da
jibo <jibo@outlook.com>
2021-09-24 11:02:18 +0800

    Add clone row for zalando receipt

--- src/service/Zalando.php
+++ src/service/Zalando.php
@@ -11,27 +11,56 @@ class Zalando
     function franceReceipt(string $save, string $input)
     {
         try {
-            $results = [];
             if (is_file($input)) {
                 writeLog('read file: ' . $input);
+
+                $data = [];
+                $headers = [];
+                $results = [];
+
                 $reader = new \SpreadsheetReader($input);
                 $reader->ChangeSheet(0);
-                $headers = $reader->current();
+                while ($reader->valid()) {
+                    $line = $reader->current();
+                    if ($headers) {
+                        $orderNumber = $line[$headers['order_number']];
+                        $table = [
+                            'product_sku' => $line[$headers['product_sku']],
+                            'product_name' => $line[$headers['product_name']],
+                            'quantity' => $line[$headers['quantity']],
+                            'unit_price' => $line[$headers['unit_price']],
+                            'line_price' => $line[$headers['line_price']],
+                        ];
+                        if (isset($data[$orderNumber])) {
+                            $data[$orderNumber]['table'][] = $table;
+                        } else {
+                            $line['table'] = [$table];
+                            $data[$orderNumber] = $line;
+                        }
+                    } else {
+                        $headers = array_flip($line);
+                    }
+                    $reader->next();
+                }
+
+                unset($headers['product_sku']);
+                unset($headers['product_name']);
+                unset($headers['quantity']);
+                unset($headers['unit_price']);
+                unset($headers['line_price']);
 
                 Settings::setCompatibility(false);
-
-                $reader->next();
-                while ($reader->valid()) {
+                foreach ($data as $k => $v) {
                     $template = new TemplateProcessor(ROOT . '/resources/template.docx');
-                    $data = $reader->current();
-                    foreach ($headers as $k => $v) {
-                        $template->setValue($v, $data[$k]);
+                    $template->cloneRowAndSetValues('product_sku', $v['table']);
+                    unset($v['table']);
+                    foreach ($headers as $name => $idx) {
+                        $template->setValue($name, $v[$idx]);
                     }
-                    $result = '/tmp/zalando_france_' . $reader->key() . '.docx';
+                    $result = '/tmp/' . $k . '_' . bin2hex(random_bytes(2)) . '_invoice.docx';
                     writeLog('Saving ' . $result);
                     $template->saveAs($result);
                     $results[] = $result;
-                    $reader->next();
                 }
 
                 $archive = new ZipArchive();
--- test/service/WordTest.php
+++ test/service/WordTest.php
@@ -11,25 +11,52 @@ class WordTest extends TestCase
     function testTemplate()
     {
         writeLog('test template');
+        $data = [];
+        $headers = [];
         $reader = new \SpreadsheetReader(ROOT . '/resources/template.xlsx');
         $reader->ChangeSheet(0);
-        $headers = $reader->current();
-
-        Settings::setCompatibility(false);
-
-        $reader->next();
         while ($reader->valid()) {
-            $template = new TemplateProcessor(ROOT . '/resources/template.docx');
-            $data = $reader->current();
-            foreach ($headers as $k => $v) {
-                $template->setValue($v, $data[$k]);
+            $line = $reader->current();
+            if ($headers) {
+                $orderNumber = $line[$headers['order_number']];
+                $table = [
+                    'product_sku' => $line[$headers['product_sku']],
+                    'product_name' => $line[$headers['product_name']],
+                    'quantity' => $line[$headers['quantity']],
+                    'unit_price' => $line[$headers['unit_price']],
+                    'line_price' => $line[$headers['line_price']],
+                ];
+                if (isset($data[$orderNumber])) {
+                    $data[$orderNumber]['table'][] = $table;
+                } else {
+                    $line['table'] = [$table];
+                    $data[$orderNumber] = $line;
+                }
+            } else {
+                $headers = array_flip($line);
             }
-            $result = '/tmp/zalando_france_' . $reader->key() . '.docx';
-            writeLog('Saving ' . $result);
-            $template->saveAs($result);
-            $results[] = $result;
             $reader->next();
         }
+
+        unset($headers['product_sku']);
+        unset($headers['product_name']);
+        unset($headers['quantity']);
+        unset($headers['unit_price']);
+        unset($headers['line_price']);
+
+        Settings::setCompatibility(false);
+        foreach ($data as $k => $v) {
+            $template = new TemplateProcessor(ROOT . '/resources/template.docx');
+            $template->cloneRowAndSetValues('product_sku', $v['table']);
+            unset($v['table']);
+            foreach ($headers as $name => $idx) {
+                $template->setValue($name, $v[$idx]);
+            }
+            $result = '/tmp/' . $k . '_invoice.docx';
+            writeLog('Saving ' . $result);
+            $template->saveAs($result);
+        }
+
         $this->assertTrue(true);
     }
 }
